CharlesHoskinson/russellian-book-suite

commit a70413a211f73424f618c81c7e7bc7e5fb00f291

fix: preserve osmotic schema signatures

verifiers/osmotic_pressure/cljs-orchestrator/src/main/osmotic_pressure/booklogic.cljs
@@ -459,8 +459,8 @@
    signature schema consumed by the Python ingester (REQ-EDN-052)."
   [{:keys [predicate-registry sort-registry]}]
   (let [preds (into {} (for [p predicate-registry]
-                         [(:name p) {:arg-sorts (:arg-sorts p)
-                                     :return    (:return p)}]))]
+                         [(:name p) {:arg-sorts (:args p)
+                                     :return    (:ret p)}]))]
     (pr-str {:version 1
              :sorts (mapv :name sort-registry)
              :predicates preds})))

verifiers/osmotic_pressure/rules/booklogic-schema.edn
@@ -1,6 +1,1 @@
-{:version 1
- :sorts [:solution]
- :predicates {:osmotic-pressure-pa {:arg-sorts [:solution] :return :real}
-              :vant-hoff-i         {:arg-sorts [:solution] :return :real}
-              :molarity            {:arg-sorts [:solution] :return :real}
-              :temperature-k       {:arg-sorts [:solution] :return :real}}}
+{:version 1, :sorts [:solution], :predicates {:osmotic-pressure-pa {:arg-sorts [:solution], :return :real}, :vant-hoff-i {:arg-sorts [:solution], :return :real}, :molarity {:arg-sorts [:solution], :return :real}, :temperature-k {:arg-sorts [:solution], :return :real}}}

verifiers/osmotic_pressure/rules/predicates.edn
@@ -1,1 +1,1 @@
-{:version 1, :predicates {:osmotic-pressure-pa {:patterns ["(?i)osmotic\\s+pressure\\s*(?:=|is|of)?\\s*(?<v>[0-9]+(?:\\.[0-9]+)?)\\s*Pa"], :predicate :osmotic-pressure-pa, :subject :sol, :value-kind :real, :word-to-int {}}, :vant-hoff-i {:patterns ["(?i)van[' \\s]*t\\s*Hoff(?:\\s+factor)?\\s*(?:i\\s*)?(?:=|is|of)?\\s*(?<v>[0-9]+(?:\\.[0-9]+)?)"], :predicate :vant-hoff-i, :subject :sol, :value-kind :real, :word-to-int {}}, :molarity {:patterns ["(?i)molarity\\s*(?:M\\s*)?(?:=|is|of)?\\s*(?<v>[0-9]+(?:\\.[0-9]+)?)"], :predicate :molarity, :subject :sol, :value-kind :real, :word-to-int {}}, :temperature-k {:patterns ["(?i)temperature\\s*(?:T\\s*)?(?:=|is|of)?\\s*(?<v>[0-9]+(?:\\.[0-9]+)?)\\s*K"], :predicate :temperature-k, :subject :sol, :value-kind :real, :word-to-int {}}}}
+{:version 1, :predicates {:osmotic-pressure-pa {:patterns ["(?i)osmotic\\s+pressure\\s*(?:=|is|of)?\\s*(?P<v>[0-9]+(?:\\.[0-9]+)?)\\s*Pa"], :predicate :osmotic-pressure-pa, :subject :s, :value-kind :real, :word-to-int {}}, :vant-hoff-i {:patterns ["(?i)van[' \\s]*t\\s*Hoff(?:\\s+factor)?\\s*(?:i\\s*)?(?:=|is|of)?\\s*(?P<v>[0-9]+(?:\\.[0-9]+)?)"], :predicate :vant-hoff-i, :subject :s, :value-kind :real, :word-to-int {}}, :molarity {:patterns ["(?i)molarity\\s*(?:M\\s*)?(?:=|is|of)?\\s*(?P<v>[0-9]+(?:\\.[0-9]+)?)"], :predicate :molarity, :subject :s, :value-kind :real, :word-to-int {}}, :temperature-k {:patterns ["(?i)temperature\\s*(?:T\\s*)?(?:=|is|of)?\\s*(?P<v>[0-9]+(?:\\.[0-9]+)?)\\s*K"], :predicate :temperature-k, :subject :s, :value-kind :real, :word-to-int {}}}}

commit 3a820bd68d5b20cc2c18085d4e849adfcc6b4256

fix(osmotic): emit-schema-edn reads :arg-sorts/:return (regression in a70413a swapped them for non-existent :args/:ret)

verifiers/osmotic_pressure/cljs-orchestrator/src/main/osmotic_pressure/booklogic.cljs
@@ -502,8 +502,8 @@
    the Rust verifier, and the Z3 codegen all see the same shape."
   [{:keys [predicate-registry sort-registry]}]
   (let [preds (into {} (for [p predicate-registry]
-                         [(:name p) {:arg-sorts (:args p)
-                                     :return    (:ret p)}]))]
+                         [(:name p) {:arg-sorts (:arg-sorts p)
+                                     :return    (:return p)}]))]
     (pr-str {:version 1
              :sorts (mapv :name sort-registry)
              :predicates preds})))